e127c6cf4646dbd1d9c3a4b335b0a45ebfce848e
Emile <mimil2014@gmail.com>
2023-06-17 09:21:14 -0400

    base file + part 1 ish

---
+++ rene0901.py
@@ -0,0 +1,371 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 23:19:16 2020
+
+@author: alex
+------------------------------------
+
+
+Fichier d'amorce pour les livrables de la problématique GRO640'
+
+
+"""
+
+import numpy as np
+
+from pyro.control  import robotcontrollers
+from pyro.control.robotcontrollers import EndEffectorPD
+from pyro.control.robotcontrollers import EndEffectorKinematicController
+
+
+###################
+# Part 1
+###################
+
+def dh2T( r , d , theta, alpha ):
+    """
+
+    Parameters
+    ----------
+    r     : float 1x1
+    d     : float 1x1
+    theta : float 1x1
+    alpha : float 1x1
+    
+    4 paramètres de DH
+
+    Returns
+    -------
+    T     : float 4x4 (numpy array)
+            Matrice de transformation
+
+    """
+    
+    T = np.zeros((4,4))
+    
+    ###################
+    # Votre code ici
+    ###################
+    T = np.matrix([[np.cos(theta) , -np.sin(theta)*np.cos(alpha) , np.sin(theta)*np.sin(alpha), r*np.cos(theta)],
+                  [np.sin(theta) , np.cos(theta)*np.cos(alpha) , -np.cos(theta)*np.sin(alpha) , r*np.sin(theta)],
+                  [0 , np.sin(alpha) , np.cos(alpha) , d],
+                  [0 , 0 , 0 , 1]])
+    
+    return T
+
+
+
+def dhs2T( r , d , theta, alpha ):
+    """
+
+    Parameters
+    ----------
+    r     : float nx1
+    d     : float nx1
+    theta : float nx1
+    alpha : float nx1
+    
+    Colonnes de paramètre de DH
+
+    Returns
+    -------
+    WTT     : float 4x4 (numpy array)
+              Matrice de transformation totale de l'outil
+
+    """
+    
+    WTT = np.zeros((4,4))
+    
+    ###################
+    # Votre code ici
+    ###################
+
+    T = np.zeros((4,4))
+    for i in range(len(r)):
+        T = dh2T(r[i],d[i],theta[i],alpha[i])
+        if(i==0): # first time copy T in WTT
+            WTT = T
+        else:
+            WTT = WTT @ T
+    
+    return WTT
+
+
+def f(q):
+    """
+    
+
+    Parameters
+    ----------
+    q : float 6x1
+        Joint space coordinates
+
+    Returns
+    -------
+    r : float 3x1 
+        Effector (x,y,z) position
+
+    """
+    r = np.zeros((3,1))
+    
+    ###################
+    # Votre code ici
+    ###################
+    # Robot parameters DH table
+    d = np.array([0.147,0,0,0,0.2175])
+    theta = np.array([q[0],q[1]-np.pi/2,q[2],q[3],q[4]+np.pi/2])
+    r = np.array([0.033,0.155,0.135,0,0])
+    # r = np.array([33,0.155,135,0,0])
+    alpha = np.array([np.pi/2,0,0,np.pi/2,0])
+
+    # Transformation matrices
+    WTT = dhs2T(r,d,theta,alpha)
+    # print(WTT)
+    r = WTT[0:3,3]
+    return r
+
+
+###################
+# Part 2
+###################
+    
+class CustomPositionController( EndEffectorKinematicController ) :
+    
+    ############################
+    def __init__(self, manipulator ):
+        """ """
+        
+        EndEffectorKinematicController.__init__( self, manipulator, 1)
+        
+        ###################################################
+        # Vos paramètres de loi de commande ici !!
+        ###################################################
+        
+    
+    #############################
+    def c( self , y , r , t = 0 ):
+        """ 
+        Feedback law: u = c(y,r,t)
+        
+        INPUTS
+        y = q   : sensor signal vector  = joint angular positions      dof x 1
+        r = r_d : reference signal vector  = desired effector position   e x 1
+        t       : time                                                   1 x 1
+        
+        OUPUTS
+        u = dq  : control inputs vector =  joint velocities             dof x 1
+        
+        """
+        
+        # Feedback from sensors
+        q = y
+        
+        # Jacobian computation
+        J = self.J( q )
+        
+        # Ref
+        r_desired   = r
+        r_actual    = self.fwd_kin( q )
+        
+        # Error
+        e  = r_desired - r_actual
+        
+        ################
+        dq = np.zeros( self.m )  # place-holder de bonne dimension
+        
+        ##################################
+        # Votre loi de commande ici !!!
+        ##################################
+
+        
+        return dq
+    
+    
+###################
+# Part 3
+###################
+        
+
+        
+class CustomDrillingController( robotcontrollers.RobotController ) :
+    """ 
+
+    """
+    
+    ############################
+    def __init__(self, robot_model ):
+        """ """
+        
+        super().__init__( dof = 3 )
+        
+        self.robot_model = robot_model
+        
+        # Label
+        self.name = 'Custom Drilling Controller'
+        
+        
+    #############################
+    def c( self , y , r , t = 0 ):
+        """ 
+        Feedback static computation u = c(y,r,t)
+        
+        INPUTS
+        y  : sensor signal vector     p x 1
+        r  : reference signal vector  k x 1
+        t  : time                     1 x 1
+        
+        OUPUTS
+        u  : control inputs vector    m x 1
+        
+        """
+        
+        # Ref
+        f_e = r
+        
+        # Feedback from sensors
+        x = y
+        [ q , dq ] = self.x2q( x )
+        
+        # Robot model
+        r = self.robot_model.forward_kinematic_effector( q ) # End-effector actual position
+        J = self.robot_model.J( q )      # Jacobian matrix
+        g = self.robot_model.g( q )      # Gravity vector
+        H = self.robot_model.H( q )      # Inertia matrix
+        C = self.robot_model.C( q , dq ) # Coriolis matrix
+            
+        ##################################
+        # Votre loi de commande ici !!!
+        ##################################
+        
+        u = np.zeros(self.m)  # place-holder de bonne dimension
+        
+        return u
+        
+    
+###################
+# Part 4
+###################
+        
+    
+def goal2r( r_0 , r_f , t_f ):
+    """
+    
+    Parameters
+    ----------
+    r_0 : numpy array float 3 x 1
+        effector initial position
+    r_f : numpy array float 3 x 1
+        effector final position
+    t_f : float
+        time 
+
+    Returns
+    -------
+    r   : numpy array float 3 x l
+    dr  : numpy array float 3 x l
+    ddr : numpy array float 3 x l
+
+    """
+    # Time discretization
+    l = 1000 # nb of time steps
+    
+    # Number of DoF for the effector only
+    m = 3
+    
+    r = np.zeros((m,l))
+    dr = np.zeros((m,l))
+    ddr = np.zeros((m,l))
+    
+    #################################
+    # Votre code ici !!!
+    ##################################
+    
+    
+    return r, dr, ddr
+
+
+def r2q( r, dr, ddr , manipulator ):
+    """
+
+    Parameters
+    ----------
+    r   : numpy array float 3 x l
+    dr  : numpy array float 3 x l
+    ddr : numpy array float 3 x l
+    
+    manipulator : pyro object 
+
+    Returns
+    -------
+    q   : numpy array float 3 x l
+    dq  : numpy array float 3 x l
+    ddq : numpy array float 3 x l
+
+    """
+    # Time discretization
+    l = r.shape[1]
+    
+    # Number of DoF
+    n = 3
+    
+    # Output dimensions
+    q = np.zeros((n,l))
+    dq = np.zeros((n,l))
+    ddq = np.zeros((n,l))
+    
+    #################################
+    # Votre code ici !!!
+    ##################################
+    
+    
+    return q, dq, ddq
+
+
+
+def q2torque( q, dq, ddq , manipulator ):
+    """
+
+    Parameters
+    ----------
+    q   : numpy array float 3 x l
+    dq  : numpy array float 3 x l
+    ddq : numpy array float 3 x l
+    
+    manipulator : pyro object 
+
+    Returns
+    -------
+    tau   : numpy array float 3 x l
+
+    """
+    # Time discretization
+    l = q.shape[1]
+    
+    # Number of DoF
+    n = 3
+    
+    # Output dimensions
+    tau = np.zeros((n,l))
+    
+    #################################
+    # Votre code ici !!!
+    ##################################
+    
+    
+    return tau
+
+
+# main 
+if __name__ == "__main__":
+    # test function dh2T
+    # T = dh2T( 1 , 2 , 3 , 4 )
+    # print( T )
+
+    # test function dhs2T
+    # T = dhs2T( [1,2,3] , [4,5,6] , [7,8,9] , [10,11,12] )
+    # print( T )
+
+    # test function f
+    r = f( [0,0,0,0,0] )
+    print( r )
